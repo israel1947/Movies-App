9fcf88f7ffb278ece0b7a55086d9b3f497140377
israel1947 <israeldadidsdvsf@gmail.com>
2022-05-13 13:32:48 -0500

    the global variables of the application are added

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,8 @@
 export const environment = {
   production: false,
   api_key:'d0dfa05a65b3a18bd432149a75bb9f84',
+  url:'https://api.themoviedb.org/4',
+  imagen_path:'https://image.tmdb.org/t/p',
 };
 
 /*
